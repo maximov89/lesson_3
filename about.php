e0fa604dcaba662fe31ab2dbeccaf836a1fe408c
maximov89 <37774793+maximov89@users.noreply.github.com>
2018-04-05 20:10:34 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,35 @@
+<?php
+$continents = array(
+	'africa' => array(
+		'lemuriformes ',
+		'cercopithecinae ',
+		'panthera leo'
+	),
+	'eurasia' => array(
+		'rangifer tarandus',
+		'elephantidae ',
+		'felis tigris '
+	),
+	'south America' => array(
+		'puma concolor',
+		'vultur gryphus',
+		'phoenicopterus'
+	),
+	'australia' => array(
+		'vombatidae',
+		'macropodidae',
+		'phascolarctos cinereus '
+	),
+	'north America' => array(
+		'vulpes lagopus',
+		'lemmini',
+		'lepus arcticus'
+	),
+	'antarctica' => array(
+		'aptenodytes forsteri ',
+		'phocidae',
+		'procellariidae'
+	)
+);
+echo $continents['africa'][2];
+?>
